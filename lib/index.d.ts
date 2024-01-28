79aa972258b2d3cb4b965c001d3866e3ca651260
hcl-z <1401859664@qq.com>
2024-01-29 00:37:58 +0800

    fix: bad case

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,8 @@
 export interface Options {
     importName?: string;
-    needClassnames?: boolean;
+    supportClassnames?: boolean;
+    ignorePrefix?: (string | RegExp)[];
+    exactMatch?: boolean;
 }
 /**
  * 1. className="class1"
@@ -11,4 +13,14 @@ export interface Options {
  * 6. className={isShow?'show':'hide'}
  */
 export declare function transformClassToCSSModule(sourceCode: string, options?: Options): string;
+/**
+ * 1. className={style['class1']}
+ * 2. className={classnames(style['class1'],style['class2'])}
+ * 3. className={'class1 class2'}
+ * 4. className={style[value1]}}
+ * 5. className={classnames(style[value1],style['class1']}
+ * 6. className={isShow?style['show']:style['hide']}
+ * 7. className={test+'class'}
+ */
+export declare function transformCSSModuleToClass(sourceCode: string, options?: Options): string;
 //# sourceMappingURL=index.d.ts.map
